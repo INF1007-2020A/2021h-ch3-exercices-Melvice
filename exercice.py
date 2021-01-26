04b954c00b5d4dbce569581d62a899d488ca33fc
Melvice <melvice-junior.guimfack@polymtl.ca>
2021-01-26 14:34:49 -0500

    update

--- exercice.py
+++ exercice.py
@@ -17,8 +17,8 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    degré=angle_degs+ (angle_mins/60 + angle_secs/3600)
-    return degré*(math.pi/180)
+    degré=angle_degs+ (angle_mins/60) + (angle_secs/3600)
+    return math.radians(degré)
 
 
 def to_degrees(angle_rads: float) -> tuple:
